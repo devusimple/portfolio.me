0fe63f7c62d33728b20fad1e2a1e131e85afc40f
Mehedi Hasan <nahidhasan195446@gmail.com>
2024-10-17 14:52:43 +0600

    Update page.tsx
    Comments out Navbar and About

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-//import About from "@/components/About";
+import About from "@/components/About";
 import Blog from "@/components/Blog";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
-//import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
@@ -15,10 +15,10 @@ export default function Home() {
   console.log(activeSection)
   return (
     <div className="bg-gray-100 min-h-screen">
-      {/* <Navbar activeSection={activeSection} /> */}
+      <Navbar activeSection={activeSection} />
       <main>
         <Hero setActiveSection={setActiveSection} />
-        {/* <About setActiveSection={setActiveSection} /> */}
+        <About setActiveSection={setActiveSection} />
         <Skills setActiveSection={setActiveSection} />
         <Projects setActiveSection={setActiveSection} />
         <Blog setActiveSection={setActiveSection} />
