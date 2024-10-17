d57bf622a6cee81f33bcda78682fa73e23844d45
Mehedi Hasan <nahidhasan195446@gmail.com>
2024-10-17 14:35:36 +0600

    Update page.tsx
    Navbar import comment

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Blog from "@/components/Blog";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
+//import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Testimonials from "@/components/Testimonials";
